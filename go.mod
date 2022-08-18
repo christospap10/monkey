17ad4bfd1f545279ec866ab34d749e06219db9b1
christospap10 <xristospapotis@gmail.com>
2022-08-18 21:09:53 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/christospap10/monkey
+
+go 1.18
---
+++ lexer/lexer_test.go
@@ -0,0 +1,14 @@
+package lexer
+
+import (
+	"testing"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{}
+}
